f9ae12ee1283a7e3b5d45ce7a67901f8f3e96fe2
panlianghong <820604905@qq.com>
2019-12-20 17:35:17 +0800

    首页

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
 
 Vue.use(VueRouter)
 
 export const constantRouterMap = [{
     path: '/',
-    name: 'home',
-    component: Home
+    component: () => import('@/views/Homepage'),
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import( /* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'login',
+    component: (resolve) => {
+      require(['@/views/login/index'], resolve)
+    },
   }
 ]
 
---
+++ src/views/login/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="login">
+    <span>login</span>
+  </div>
+</template>
+<style lang="scss" scoped>
+#login {
+  position: absolute;
+  width: 100%;
+  span {
+    box-sizing: border-box;
+    display: block;
+    width: 100px;
+    margin: 20px;
+  }
+}
+</style>
